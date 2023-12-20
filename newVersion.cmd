41ee52ba23628060a523c0b86840deba673cdf32
Tomas Kupka <tomas.kupka@mgm-tp.com>
2023-12-20 11:31:52 +0100

    release job fixes

--- newVersion.cmd
+++ newVersion.cmd
@@ -1,3 +1,3 @@
 echo Setting version to %1
-mvn versions:set -DnewVersion=%1
+mvn versions:set -DnewVersion=%1-SNAPSHOT
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=%1
--- newVersion.sh
+++ newVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo Setting version to $1
-mvn versions:set -DnewVersion=$1
+mvn versions:set -DnewVersion=$1-SNAPSHOT
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1
